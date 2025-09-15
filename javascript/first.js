ab50f3121f6cde74006bd3a0cb2ca973a772b827
santhoshpureti2059 <santhoshpureti2059@gmail.com>
2025-09-15 10:18:53 +0530

    demo

---
+++ javascript/first.js
@@ -0,0 +1,168 @@
+/*const student = {
+  name: "rahul ",
+  age: 20,
+  cgpa: 8.2,
+  ispass: true,
+}; 
+student["age"] + 5;
+console.log(student["age"]);
+
+
+const product = {
+    name: "parker jotter standard ct ball pen (black)",
+    price: 270,
+    color:"black",
+     rating:4,
+     offer:5,
+      
+}*
+console.log(product);
+
+const profile={
+    website:"instagram",
+    acount_name:"shradhakhapra",
+    posts:193,
+    followers:"569k",
+    following:4,
+    
+
+}
+console.log(profile);
+
+let a, b, c;
+c = a + b;
+console.log(c);
+
+let a = 10;
+b = 20;
+c = a + b;
+console.log("sum is :", c);
+
+
+let a,b,c;
+a=prompt("enter a number :");
+b=prompt("enter a number2");
+c=Number(a)+Number(b);
+console.log("result c:",c);
+
+// area and perimeter of sqaure 
+//area = s*s  and perimeter = 4*s
+let a,p,s;
+s = Number(prompt("enter side of square: "));
+a=s*Number(s);
+p=4*Number(s);
+console.log("area:",a);
+console.log("perimeter:",p);
+
+let a,r;
+r = prompt("enter radius:")
+const pi=3.14;
+a=4*pi*r*r;
+console.log("area of circle: ",a);
+
+
+let v,r,h;
+r=prompt("enter radius: ");
+h=prompt("enter height: ");
+const pi=3.14;
+v=pi*r*r*h;
+console.log("volume of a circle: ",v);
+
+
+let c,f;
+f=prompt("enter fahrenheit: ");
+c=5/9*(f-32);
+console.log("celcius =",c);
+
+
+let a,b,h;
+b=prompt("enter breadth: ");
+h=prompt("enter height: ");
+a=1/2*b*h;
+console.log("area of triangle=",a);
+
+
+let A,p,s,a,b,c;
+a=prompt("enter a value is : ");
+b=prompt("enter b value  is :");
+c=prompt("enter c value is :");
+p=a+b+c;
+s=(a+b+c)/2;
+A= (s*(s-a)*(s-b)*(s-c)**1/2);
+console.log("perimeter =",p);
+console.log("s=",s);
+console.log("Area=",A);
+/*
+let T,m,g,h,v;
+m=prompt("m");
+constg = 9.8;
+h=prompt("h");
+v=prompt("v");
+
+T = m*g*h  +  1/2*m*v*v;
+console.log("Tme =",T);
+
+
+
+//unary  operator
+let a=2;
+let b=3; 
+console.log("a=",a,"&b =",b);
+a=a--;
+console.log("a=",a++);
+
+
+//assignment operators
+let a=5;
+let b=9;
+
+a+=4;
+console.log(a);
+
++=  -=  
+
+
+//comparision opearator 
+
+let a=5 ;
+let b=3;
+
+
+console.log("5 == 3",a==b);
+console.log("5  != 3",a!=b);
+console.log("5 === 3",a===b);
+
+
+
+
+
+let a = "10",
+  b = "20.5",
+  c;
+c = Number(a) + Number(b);
+console.log("result:", c);*/
+
+
+   
+let a = "10",b = "20.5", c;
+c = Number(a) + Number(b);
+console.log("result:", c);
+
+
+   
+
+
+
+
+
+
+
+ 
+
+   
+
+
+
+
+
+
